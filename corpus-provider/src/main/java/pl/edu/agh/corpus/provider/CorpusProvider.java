866250223f600f9bd539be675534972e01608a67
slewicki <r2mzes@gmail.com>
2018-06-10 18:16:09 +0200

    Method for getting posts filtered by country group added

--- corpus-provider/src/main/java/pl/edu/agh/corpus/provider/CorpusProvider.java
+++ corpus-provider/src/main/java/pl/edu/agh/corpus/provider/CorpusProvider.java
@@ -1,29 +1,38 @@
 package pl.edu.agh.corpus.provider;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.*;
+import java.util.stream.Collectors;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
+
 import pl.edu.agh.corpus.provider.model.Post;
 import pl.edu.agh.corpus.provider.model.Thread;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
 public class CorpusProvider {
-	private static final String PATH_TO_CORPUS = "news_corpus";
+	private static final String DEFAULT_PATH_TO_CORPUS = "news_corpus";
+
+	private String pathToCorpus;
+
+	public CorpusProvider() {
+		this.pathToCorpus = DEFAULT_PATH_TO_CORPUS;
+	}
+
+	public CorpusProvider(String pathToCorpus) {
+		this.pathToCorpus = pathToCorpus;
+	}
 
 	public List<Post> getPosts() throws FileNotFoundException {
 		List<Post> posts = new ArrayList<>();
 
-		File dir = new File(PATH_TO_CORPUS);
+		File dir = new File(DEFAULT_PATH_TO_CORPUS);
 		File[] directoryListing = dir.listFiles();
 		Type listType = new TypeToken<List<Post>>() {
 		}.getType();
@@ -40,8 +49,46 @@ public class CorpusProvider {
 		return posts;
 	}
 
+	public Map<Set<String>, List<Post>> getPostsByCountryGroup(int countryAmountPerGroup) throws FileNotFoundException {
+		List<Post> allPosts = getPosts();
+		Set<String> allCountries = allPosts.stream().map(Post::getThread).map(Thread::getCountry).distinct()
+				.collect(Collectors.toSet());
+
+		Set<Set<String>> allCountryPowerSet = powerSet(allCountries);
+		Set<Set<String>> allCountryCombinations = allCountryPowerSet.stream()
+				.filter(set -> set.size() == countryAmountPerGroup).collect(Collectors.toSet());
+
+		Map<Set<String>, List<Post>> postsByCountryGroup = new HashMap<>();
+
+		for (Set<String> countryGroup : allCountryCombinations) {
+			postsByCountryGroup.put(countryGroup, allPosts.stream()
+					.filter(post -> countryGroup.contains(post.getThread().getCountry())).collect(Collectors.toList()));
+		}
+		return postsByCountryGroup;
+	}
+
+	public <T> Set<Set<T>> powerSet(Set<T> originalSet) {
+		Set<Set<T>> sets = new LinkedHashSet<Set<T>>();
+		if (originalSet.isEmpty()) {
+			sets.add(new LinkedHashSet<T>());
+			return sets;
+		}
+		List<T> list = new ArrayList<T>(originalSet);
+		T head = list.get(0);
+		Set<T> rest = new LinkedHashSet<T>(list.subList(1, list.size()));
+		for (Set<T> set : powerSet(rest)) {
+			Set<T> newSet = new LinkedHashSet<T>();
+			newSet.add(head);
+			newSet.addAll(set);
+			sets.add(newSet);
+			sets.add(set);
+		}
+		return sets;
+	}
+
 	public static void main(String[] args) throws FileNotFoundException {
 		CorpusProvider app = new CorpusProvider();
-		app.getPosts();
+		app.getPostsByCountryGroup(3);
+		System.out.println("DONE");
 	}
 }
